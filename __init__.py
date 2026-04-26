1e0f18a2f800bf9b7d5d0aae71b23bc2147cd31a
NAJEM-BRAHIM <SAHARA.INFO.SERVICES@GMAIL.COM>
2026-04-26 20:26:22 +0100

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,34 +1,6 @@
-# -*- coding: utf-8 -*-
-
-from odoo import api, fields, models, tools, _
-
-
-class AccountMoveLine(models.Model):
-
-    _inherit = 'account.move.line'
-
-    virtual_box = fields.Integer('Boxes')
-
-
-class AccountMove(models.Model):
-
-    _inherit = 'account.move'
-
-    total_virtual_box = fields.Integer(
-        string='Total Cajas',
-        compute='_compute_totals_virtualbox',
-        store=True,
-    )
-    total_quantity = fields.Float(
-        string='Total Unidades',
-        compute='_compute_totals_virtualbox',
-        store=True,
-        digits='Product Unit of Measure',
-    )
-
-    @api.depends('invoice_line_ids.virtual_box', 'invoice_line_ids.quantity')
-    def _compute_totals_virtualbox(self):
-        for move in self:
-            lines = move.invoice_line_ids.filtered(lambda l: l.display_type not in ('line_section', 'line_note'))
-            move.total_virtual_box = sum(lines.mapped('virtual_box'))
-            move.total_quantity = sum(lines.mapped('quantity'))
+id,name,model_id:id,group_id:id,perm_read,perm_write,perm_create,perm_unlink
+access_hr_azk_machine,Access to Machine,model_azk_machine,hr_attendance.group_hr_attendance_officer,1,1,1,1
+access_hr_azk_attendance,Access to Attendance,model_azk_machine_attendance,hr_attendance.group_hr_attendance_officer,1,1,1,1
+access_hr_azk_attendance_report,Access to Attendance reports,model_azk_report_daily_attendance,hr_attendance.group_hr_attendance_officer,1,1,1,1
+access_azk_machine_user_create,azk_machine_user_create,model_azk_machine_user_create,hr_attendance.group_hr_attendance_officer,1,1,1,1
+access_azk_machine_user_delete,azk_machine_user_delete,model_azk_machine_user_delete,hr_attendance.group_hr_attendance_officer,1,1,1,1
