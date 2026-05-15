9bd7f01eee1ec4a1ec0d1d66394c6dffcec0a086
NAJEM-BRAHIM <SAHARA.INFO.SERVICES@GMAIL.COM>
2026-05-15 20:15:47 +0100

    [ADD] Módulo azk_zkteco_attendance_v19 para integración con máquinas ZKTeco en Odoo 19

---
+++ azk_zkteco_attendance_v19/models/machine_analysis.py
@@ -0,0 +1,95 @@
+from odoo import tools
+from odoo import models, fields, api, _
+
+
+class ZkMachineAttendance(models.Model):
+    _name = 'azk.machine.attendance'
+    _description = 'AZK Machine Attendance'
+
+    employee_id = fields.Many2one('hr.employee', string="Employee")
+    device_id = fields.Char(string='Attendance ID')
+    punch_type = fields.Selection([
+        ('0', 'Check In'),
+        ('1', 'Check Out'),
+        ('2', 'Break Out'),
+        ('3', 'Break In'),
+        ('4', 'Overtime In'),
+        ('5', 'Overtime Out'),
+    ], string='Punching Type')
+
+    attendance_type = fields.Selection([
+        ('1', 'Finger'),
+        ('15', 'Face'),
+        ('2', 'Type_2'),
+        ('3', 'Password'),
+        ('4', 'Card'),
+    ], string='Category')
+
+    punching_time = fields.Datetime(string='Punching Time')
+    address_id = fields.Many2one('res.partner', string='Working Address')
+    import_status = fields.Selection([
+        ('imported', 'Imported'),
+        ('skipped', 'Skipped'),
+    ], string='Import Status')
+
+
+class ReportZkDevice(models.Model):
+    _name = 'azk.report.daily.attendance'
+    _description = 'AZK Report Daily Attendance'
+    _auto = False
+    _order = 'punching_day desc'
+
+    name = fields.Many2one('hr.employee', string='Employee')
+    punching_day = fields.Datetime(string='Date')
+    address_id = fields.Many2one('res.partner', string='Working Address')
+
+    attendance_type = fields.Selection([
+        ('1', 'Finger'),
+        ('15', 'Face'),
+        ('2', 'Type_2'),
+        ('3', 'Password'),
+        ('4', 'Card'),
+    ], string='Category')
+
+    punch_type = fields.Selection([
+        ('0', 'Check In'),
+        ('1', 'Check Out'),
+        ('2', 'Break Out'),
+        ('3', 'Break In'),
+        ('4', 'Overtime In'),
+        ('5', 'Overtime Out'),
+    ], string='Punching Type')
+
+    punching_time = fields.Datetime(string='Punching Time')
+    import_status = fields.Selection([
+        ('imported', 'Imported'),
+        ('skipped', 'Skipped'),
+    ], string='Import Status')
+
+    def init(self):
+        # v19: tools.drop_view_if_exists signature unchanged
+        tools.drop_view_if_exists(self._cr, 'azk_report_daily_attendance')
+        query = """
+            CREATE OR REPLACE VIEW azk_report_daily_attendance AS (
+                SELECT
+                    min(z.id)           AS id,
+                    z.employee_id       AS name,
+                    z.write_date        AS punching_day,
+                    z.address_id        AS address_id,
+                    z.attendance_type   AS attendance_type,
+                    z.punching_time     AS punching_time,
+                    z.punch_type        AS punch_type,
+                    z.import_status     AS import_status
+                FROM azk_machine_attendance z
+                    JOIN hr_employee e ON (z.employee_id = e.id)
+                GROUP BY
+                    z.employee_id,
+                    z.write_date,
+                    z.address_id,
+                    z.attendance_type,
+                    z.punch_type,
+                    z.punching_time,
+                    z.import_status
+            )
+        """
+        self._cr.execute(query)
